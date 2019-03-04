633c4f4f5b47bb6d6a21d75ac59ca394998ca87c
Steve Conklin <steve@conklinhouse.com>
2019-03-03 22:04:49 -0600

    Bump SD card version and finalize Joco version 2.0

--- firmware/src/system.h
+++ firmware/src/system.h
@@ -29,7 +29,7 @@
  *****************************************************************************/
 #ifndef SYSTEM_H_
 #define SYSTEM_H_
-#define VERSION_SD						33
+#define VERSION_SD						34
 
 // Various build options
 #define INCLUDE_CAPTURE true // do we include the capture game?
